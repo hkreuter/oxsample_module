3d5e7091c447751d92f8a5722de83817c1d056e1
hkreuter <heike@reuter-consulting.org>
2019-03-06 22:32:21 +0100

    Clean source/tmp on module activation and deactivation

--- Core/Module.php
+++ Core/Module.php
@@ -15,13 +15,80 @@ class Module
 {
 	const MODULE_ID = 'hkreuter/oxsample-module';
 
+	/**
+	 * Files that are not deleted.
+	 *
+	 * @var array
+	 */
+	static protected $keepThese = [
+		'.',
+		'..',
+		'.gitkeep',
+		'.htaccess'
+	];
+
+	/**
+	 * On module activation
+	 */
 	public function onActivate()
 	{
-        //TODO: implement
+		self::clearTmp();
 	}
 
+	/**
+	 * On module deactivation
+	 */
 	public function onDeactivate()
 	{
-		//TODO: implement
+		self::clearTmp();
+	}
+
+	/**
+	 * Clean temp folder content.
+	 */
+	public static function clearTmp()
+	{
+		$compileDir = (string) \OxidEsales\Eshop\Core\Registry::getConfig()->getConfigParam('sCompileDir');
+		if (is_dir($compileDir)) {
+			static::clearDirectory($compileDir);
+		}
+	}
+
+	/**
+	 * Clean temp folder content.
+	 *
+	 * @param string $clearFolderPath Sub-folder path to delete from.
+	 *
+	 * @return boolean
+	 */
+	private static function clearDirectory($directoryPath)
+	{
+		$directoryHandler = opendir($directoryPath);
+		if (!empty($directoryHandler)) {
+			while (false !== ($fileName = readdir($directoryHandler))) {
+				$filePath = $directoryPath . DIRECTORY_SEPARATOR . $fileName;
+				self::clear($fileName, $filePath);
+			}
+			closedir($directoryHandler);
+		}
+		return true;
+	}
+
+	/**
+	 * Check if resource could be deleted, then delete it's a file or
+	 * call recursive folder deletion if it's a directory.
+	 *
+	 * @param string $fileName
+	 * @param string $filePath
+	 */
+	private static function clear($fileName, $filePath)
+	{
+		if (!in_array($fileName, ['.', '..', '.gitkeep', '.htaccess'])) {
+			if (is_file($filePath)) {
+				@unlink($filePath);
+			} else {
+				self::clearDirectory($filePath);
+			}
+		}
 	}
 }
